b26300f135f37e090245b44263243205f2013cf0
André Dias <dedelcdias@gmail.com>
2024-02-21 08:06:55 -0300

    refactor: connect to mongodb before integration test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,16 +1,33 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+const MONGO_URL = 'mongodb://localhost:27017/jest'
 
 describe('SignUp Routes', () => {
   test('Should return an account on success', async () => {
-    await request(app)
-      .get('/api/signup')
-      .send({
-        name: 'André',
-        email: 'andre@mail.com',
-        password: 'senha123',
-        passwordConfirmation: 'senha123'
-      })
-      .expect(200)
+    beforeAll(async () => {
+      await MongoHelper.connect(MONGO_URL)
+    })
+
+    afterAll(async () => {
+      await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+      const accountCollection = MongoHelper.getCollection('accounts')
+      await accountCollection.deleteMany({})
+    })
+
+    test('Should return an account on success', async () => {
+      await request(app)
+        .get('/api/signup')
+        .send({
+          name: 'André',
+          email: 'andre@mail.com',
+          password: 'senha123',
+          passwordConfirmation: 'senha123'
+        })
+        .expect(200)
+    })
   })
 })
