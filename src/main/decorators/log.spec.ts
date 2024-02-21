427f7c78a10c6101f3c2f296be8ffca4cd0b41af
André Dias <dedelcdias@gmail.com>
2024-02-21 10:01:47 -0300

    test: ensure LogControllerDecorators returns the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -44,4 +44,23 @@ describe('LogController Decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        emai: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any password',
+        passwordConfirmation: 'any password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'André'
+      }
+    })
+  })
 })
