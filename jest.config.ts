bb9667abf122523338a1e152cc22098ceb5ba1f9
André Dias <dedelcdias@gmail.com>
2024-02-21 09:15:36 -0300

    feat: ensure MongoDB reconnect if client is down

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,6 @@ import type { Config } from 'jest'
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
